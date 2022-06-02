22adba2e1a4fead7379225c9e30d40088344c1ee
andezitgq <sanerhp.mail@gmail.com>
2022-06-02 19:08:20 +0300

    456

--- search.php
+++ search.php
@@ -1,65 +1,76 @@
 <?php include_once "tml/header.php"; ?>
 
-<div>
-	<div class="mx-5 my-3">
-		<div class="input-group mb-3">
-		  <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2">
-		  <button class="btn btn-outline-secondary" type="button" id="button-addon2">Button</button>
+<div class="container mx-5 my-3">
+	<h1>Search</h1>
+	<div class="input-group mb-3">
+	  <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2">
+	  <button class="btn btn-outline-secondary" type="button" id="button-addon2">Button</button>
+	</div>
+	<div class="row">
+		<div class="col-lg-4 col-md-6 col-sm-12">
+			<div class="card m-3">
+				<div class="card-body">
+					<h5 class="card-title">Oleksandra, 19</h5>
+					<p class="card-text text-success">• Online</h5>
+				</div>
+				<div class="card-field" style="background-image: url('https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500')">
+					&nbsp;
+				</div>
+			</div>
 		</div>
-		<div class="container d-flex flex-wrap justify-content-center">
-			<div class="card m-3" style="width: 18rem;">
-			  <div class="card-body">
-				<h5 class="card-title">Oleksandra, 19</h5>
-				<p class="card-text text-success">• Online</h5>
-			  </div>
-			  <div class="card-field">
-				&nbsp;
-			  </div>
+		<div class="col-lg-4 col-md-6 col-sm-12">
+			<div class="card m-3">
+				<div class="card-body">
+					<h5 class="card-title">Oleksandra, 19</h5>
+					<p class="card-text text-success">• Online</h5>
+				</div>
+				<div class="card-field" style="background-image: url('https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500')">
+					&nbsp;
+				</div>
 			</div>
-			<div class="card m-3" style="width: 18rem;">
-			  <div class="card-body">
-				<h5 class="card-title">Valeria, 28</h5>
-				<p class="card-text text-secondary">Offline</h5>
-			  </div>
-			  <div class="card-field">
-				&nbsp;
-			  </div>
+		</div>
+		<div class="col-lg-4 col-md-6 col-sm-12">
+			<div class="card m-3">
+				<div class="card-body">
+					<h5 class="card-title">Oleksandra, 19</h5>
+					<p class="card-text text-success">• Online</h5>
+				</div>
+				<div class="card-field" style="background-image: url('https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500')">
+					&nbsp;
+				</div>
 			</div>
-			<div class="card m-3" style="width: 18rem;">
-			  <div class="card-body">
-				<h5 class="card-title">Olga, 23</h5>
-				<p class="card-text text-secondary">Offline</h5>
-			  </div>
-			  <div class="card-field">
-				&nbsp;
-			  </div>
+		</div>
+		<div class="col-lg-4 col-md-6 col-sm-12">
+			<div class="card m-3">
+				<div class="card-body">
+					<h5 class="card-title">Oleksandra, 19</h5>
+					<p class="card-text text-success">• Online</h5>
+				</div>
+				<div class="card-field" style="background-image: url('https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500')">
+					&nbsp;
+				</div>
 			</div>
-			<div class="card m-3" style="width: 18rem;">
-			  <div class="card-body">
-				<h5 class="card-title">Maria, 25</h5>
-				<p class="card-text text-secondary">Offline</h5>
-			  </div>
-			  <div class="card-field">
-				&nbsp;
-			  </div>
+		</div>
+		<div class="col-lg-4 col-md-6 col-sm-12">
+			<div class="card m-3">
+				<div class="card-body">
+					<h5 class="card-title">Oleksandra, 19</h5>
+					<p class="card-text text-success">• Online</h5>
+				</div>
+				<div class="card-field" style="background-image: url('https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500')">
+					&nbsp;
+				</div>
 			</div>
-			<div class="card m-3" style="width: 18rem;">
-			  <div class="card-body">
-				<h5 class="card-title">Anastasia, 34</h5>
-				<p class="card-text text-success">• Online</h5>
-			  </div>
-			  <div class="card-field">
-				&nbsp;
-			  </div>
-			</div>
-			<div class="card m-3" style="width: 18rem;">
-			  <div class="card-body">
-				<h5 class="card-title">Sofia, 21</h5>
-				<p class="card-text text-secondary">Offline</h5>
-			  </div>
-			  <div class="card-field">
-				&nbsp;
-			  </div>
+		</div>
+		<div class="col-lg-4 col-md-6 col-sm-12">
+			<div class="card m-3">
+				<div class="card-body">
+					<h5 class="card-title">Oleksandra, 19</h5>
+					<p class="card-text text-success">• Online</h5>
+				</div>
+				<div class="card-field" style="background-image: url('https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500')">
+					&nbsp;
+				</div>
 			</div>
 		</div>
 	</div>
